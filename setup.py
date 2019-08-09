3edea7eedec97d96e19f16b54770827cbd7410bf
Andreas Roth <aroth@arsoft-online.com>
2019-08-09 09:58:40 +0200

    using submodules for child packages

--- setup.py
+++ setup.py
@@ -4,14 +4,20 @@
 import sys
 import argparse
 import urllib.request
+import errno
+import shutil
 import os
 import os.path
+from zipfile import ZipFile
+from arsoft.utils import *
 
 package_list = {
     'AdvancedTicketWorkflowPlugin':  {
         'site': 'trac-hacks',
         'repo': 'svn',
-        'repo_subdir': '1.2'
+        'repo_subdir': '1.2',
+        'pkgrepo': 'git',
+        'pkgrepo_dir': 'trac-advancedworkflow',
     },
 }
 
@@ -63,7 +69,19 @@ def get_html_title(url):
         pass
     return None
 
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
 
+def copy_and_overwrite(from_path, to_path):
+    if os.path.exists(to_path):
+        shutil.rmtree(to_path)
+    shutil.copytree(from_path, to_path)
 
 class trac_package_update_app(object):
     def __init__(self):
@@ -93,6 +111,7 @@ class trac_package_update_app(object):
 
     def _download_pkgs(self):
         self._get_latest_revisions()
+        mkdir_p(self._download_dir)
         for name, details in package_list.items():
             print('%s' % name)
             site = site_list.get(details.get('site', None), None)
@@ -105,12 +124,64 @@ class trac_package_update_app(object):
                 url += '?format=zip'
                 if site_rev is not None:
                     url += '&rev=%s' % site_rev
-                filename = name.lower() + '.zip'
-                dest = os.path.join(self._temp_dir, filename)
-                print(url)
-                urllib.request.urlretrieve(url, dest)
+                filename = name.lower()
+                if site_rev is not None:
+                    filename += '_rev%s' % site_rev
+                filename += '.zip'
+                dest = os.path.join(self._download_dir, filename)
+                if os.path.isfile(dest):
+                    os.unlink(dest)
+                #print(url)
+                download_ok = False
+                try:
+                    urllib.request.urlretrieve(url, dest)
+                    download_ok = True
+                except urllib.error.HTTPError as ex:
+                    print('HTTP error %s for %s' % (ex, url))
+                if download_ok:
+                    pkg_download_dir = os.path.join(self._download_dir, name.lower())
+                    with ZipFile(dest, 'r') as zipObj:
+                        # Extract all the contents of zip file in different directory
+                        zipObj.extractall(pkg_download_dir)
         return 0
 
+    def _update_package_repo(self):
+        mkdir_p(self._repo_dir)
+        for name, details in package_list.items():
+            pkgrepo = details.get('pkgrepo', '')
+            repo_ok = False
+            if pkgrepo == 'git':
+                pkgrepo_dir = details.get('pkgrepo_dir', None)
+                if pkgrepo_dir:
+                    repo_dir = os.path.join(self._repo_dir, pkgrepo_dir)
+                    repo_ok = os.path.isdir(repo_dir)
+                else:
+                    pkgrepo_url = details.get('pkgrepo_url', None)
+                    repo_dir = os.path.join(self._repo_dir, name.lower())
+                    if os.path.isdir(repo_dir):
+                        try:
+                            (sts, stdoutdata, stderrdata) = runcmdAndGetData(args=['git', 'pull', 'origin'])
+                        except FileNotFoundError as ex:
+                            print('Cannot execute git.', file=sys.stderr)
+                            sts = -1
+                        print(stdoutdata, stderrdata)
+                    else:
+                        try:
+                            (sts, stdoutdata, stderrdata) = runcmdAndGetData(args=['git', 'clone', pkgrepo_url, repo_dir])
+                        except FileNotFoundError as ex:
+                            print('Cannot execute git.', file=sys.stderr)
+                            sts = -1
+                        print(stdoutdata, stderrdata)
+                    repo_ok = True if sts == 0 else False
+
+            if repo_ok:
+                print('Repository %s ok' % repo_dir)
+                repo_subdir = details.get('repo_subdir', '')
+                pkg_download_dir = os.path.join(self._download_dir, name.lower(), repo_subdir)
+                if os.path.isdir(pkg_download_dir):
+                    copy_and_overwrite(pkg_download_dir, repo_dir)
+            else:
+                print('Repository %s failed' % repo_dir)
 
 
     def main(self):
@@ -120,17 +191,22 @@ class trac_package_update_app(object):
         parser = argparse.ArgumentParser(description='update/generate trac packages')
         parser.add_argument('-v', '--verbose', dest='verbose', action='store_true', help='enable verbose output of this script.')
         parser.add_argument('-l', '--list', dest='list', action='store_true', help='show list of all packages.')
-        parser.add_argument('-d', '--downlaod', dest='download', action='store_true', help='download the latest version of all packages.')
+        parser.add_argument('-d', '--download', dest='download', action='store_true', help='download the latest version of all packages.')
+        parser.add_argument('-u', '--update', dest='update', action='store_true', help='update the package repositories.')
 
         args = parser.parse_args()
         self._verbose = args.verbose
 
-        self._temp_dir = os.path.abspath(os.getcwd())
+        base_dir = os.path.abspath(os.getcwd())
+        self._download_dir = os.path.join(base_dir, 'download')
+        self._repo_dir = os.path.join(base_dir, 'repo')
 
         if args.list:
             ret = self._list()
         elif args.download:
             ret = self._download_pkgs()
+        elif args.update:
+            ret = self._update_package_repo()
         else:
             ret = 0
 
